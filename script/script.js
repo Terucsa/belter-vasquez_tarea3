5390ec5b424fb1b3744bbd12bd12ca5306d83785
Belter Vasquez <Belter Vasquez>
2022-12-27 12:42:17 -0600

    Terminando mostrar paises

--- script/script.js
+++ script/script.js
@@ -39,6 +39,11 @@ const paises = [
 
 agregarMonitores();
 
+//Variables tabala
+let headTable = document.getElementById('headTabla');
+//Variable de bodyTabla
+
+
 //funcion para agregar listeners a los botones
 function agregarMonitores() {
     //TODO
@@ -48,6 +53,35 @@ function agregarMonitores() {
 //funcion que mostrara las paises en la pagina
 function mostrarPaises() {
   //TODO
+    
+    //Funcion para mostrar body en la tabla
+    let bodyTabla = paises.map(function(paisesTod){
+        
+            return `
+            <thead>
+                <tr class="trTabla">
+                <td>Nombre:</td>
+                <td>Capital:</td>
+                <td>Idioma:</td>
+                <td>Moneda:</td>
+                </tr>
+            </thead>
+            <tbody id="">
+                <tr class="trTabla">
+                <td>${paisesTod.nombre}</td>
+                <td>${paisesTod.capital}</td>
+                <td>${paisesTod.idioma_oficial}</td>
+                <td>${paisesTod.moneda}</td>
+                </tr>
+            </tbody>
+            
+                <section class="container-fluid">
+                <img src="${paisesTod.bandera}" alt="" class"text-center">
+                </section>
+           
+            `;  
+        })
+   document.getElementById('tablaGen').innerHTML = bodyTabla;
 }
 
 //funcion que permite agregar un pais al arreglo
